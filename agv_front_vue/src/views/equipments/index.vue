d0597c75c14f8e4f1c2ddb9f6c152c8504a88215
wang <2711128665@qq.com>
2022-06-28 10:56:18 +0800

    微小改动

--- agv_front_vue/src/views/equipments/index.vue
+++ agv_front_vue/src/views/equipments/index.vue
@@ -9,13 +9,17 @@
         <el-col v-for="o in 4" :key="o" :span="5" :offset="1">
           <div style="margin-top:15px">
             <el-card style="background: #fff" :body-style="{ padding: '10px' }">
-              <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
+              <img
+                src="https://ts1.cn.mm.bing.net/th/id/R-C.deff7162fb0c016d70a41e610019b801?rik=6FZXn3hmtd7kKA&riu=http%3a%2f%2fwww.sdgssk.com%2fdata%2fupload%2f20200414%2f5e957aa38a9f3.JPG&ehk=AOMQNa9KNe9nO3nSsz97dWRmsTjVeFl0MCWHmHfWbjg%3d&risl=&pid=ImgRaw&r=0"
+                class="image"
+              >
               <div style="padding: 20px;">
-                <span class="eqname">好吃的汉堡</span>
+                <!-- <span class="eqname">好吃的汉堡</span> -->
                 <div class="bottom clearfix">
                   <!-- <time class="time">{{ currentDate }}</time>
                 <el-button size="mini" type="primary" class="button">操作按钮</el-button> -->
                   <el-descriptions :column="1" border>
+                    <el-descriptions-item label="序号" class="id">22</el-descriptions-item>
                     <el-descriptions-item label="描述">这是一个大机床</el-descriptions-item>
                     <el-descriptions-item label="状态">正在执行</el-descriptions-item>
                   </el-descriptions>
@@ -42,6 +46,9 @@ export default {
 </script>
 
 <style>
+   .el-descriptions__body .el-descriptions__table .el-descriptions-item__cell {
+    text-align: center;
+   }
    .el-header, .el-footer {
     background-color: #468be6;
     color: #333;

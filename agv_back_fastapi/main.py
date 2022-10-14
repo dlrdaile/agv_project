651e6d3c45ab3844cf86a9d2aebb9a9d08027bb5
dllr <965794928@qq.com>
2022-10-14 21:12:01 +0800

    完成环境配置文档的攥写

--- agv_back_fastapi/main.py
+++ agv_back_fastapi/main.py
@@ -51,4 +51,4 @@ async def shutdown() :
 
 if __name__ == '__main__' :
     # uvicorn.run(app='main:app', host="127.0.0.1", port=8000)
-    uvicorn.run(app='main:app',host="0.0.0.0",port=8001,debug=True,reload=True)
+    uvicorn.run(app='main:app',host="0.0.0.0",port=8001)
